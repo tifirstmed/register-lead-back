dc7a88bb49b3d855fa1bcf605f01e7f288832638
tifirstmed <tifirstmed@gmail.com>
2025-01-24 16:36:03 -0500

    Update leads.js

--- routes/leads.js
+++ routes/leads.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../models/dataModel');
 const { formatDateToPeruTime } = require('../utils/dateUtils');
 
-// Ruta para agregar un cliente
+// Ruta para agregar un cliente de meta
 router.post('/lead-register', async (req, res) => {
   const { id, nombres, apellidos, celular, correo, descripcion, producto } =
     req.body;
@@ -72,6 +72,53 @@ router.post('/lead-register-tiktok', async (req, res) => {
 
     const [result] = await connection.execute(
       'INSERT INTO leads_tiktok (id, nombres, apellidos, celular, correo, descripcion, fecha, producto) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      [
+        id,
+        nombres,
+        apellidos,
+        celular,
+        correo,
+        descripcion,
+        formattedDate,
+        producto,
+      ]
+    );
+
+    res
+      .status(201)
+      .json({ message: 'Cliente agregado exitosamente', data: result });
+  } catch (error) {
+    console.error('Error al insertar los datos', error);
+    res.status(500).json({ message: 'Error en el servidor' });
+  } finally {
+    if (connection) {
+      connection.release(); // Liberar la conexión de vuelta al pool
+    }
+  }
+});
+
+// Ruta para agregar un cliente organico
+router.post('/lead-register-organico', async (req, res) => {
+  const { id, nombres, apellidos, celular, correo, descripcion, producto } =
+    req.body;
+
+  if (!id || !nombres || !apellidos || !celular || !correo || !producto) {
+    return res
+      .status(400)
+      .json({ message: 'Todos los campos son obligatorios.' });
+  }
+
+  let connection;
+
+  try {
+    connection = await pool.getConnection();
+    const formattedDate = formatDateToPeruTime(); // Mover lógica de fecha a un helper
+    console.log(
+      `Lead tiktok por registrar, id:${id}, nombres:${nombres}, apellidos:${apellidos}, celular:${celular}, correo:${correo}, descripcion:${descripcion}, fecha:${formattedDate}, producto:${producto}`
+    );
+
+    const [result] = await connection.execute(
+      'INSERT INTO leads_organicos (id, nombres, apellidos, celular, correo, descripcion, fecha, producto) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
       [
         id,
         nombres,
